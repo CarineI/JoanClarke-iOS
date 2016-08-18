7e9b878d7de8407a24f2039978eaa230997e0706
Carine Iskander <carineiskander@carines-macbook-pro.local>
2016-08-17 18:21:54 -0700

    Initial Commit

---
+++ JoanClarke for iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JoanClarke for iOS
+//
+//  Created by Carine Iskander on 8/17/16.
+//  Copyright © 2016 Carine Iskander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
